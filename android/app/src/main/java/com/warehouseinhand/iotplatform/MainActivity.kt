2d9afb448831a73e826a224c2a45adf994704304
sinoso <tpgus123123@naver.com>
2022-01-08 16:10:59 +0900

    initial commit

---
+++ android/app/src/main/java/com/warehouseinhand/iotplatform/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.warehouseinhand.iotplatform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
